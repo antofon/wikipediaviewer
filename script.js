2c7f857ff93a02ac41ac23fa1a9c5e9c0db14bce
Anwana Ntofon <nikebillions70@gmail.com>
2017-12-06 20:49:48 -0800

    Removed unncessary code, added more properties to query string and added AJAX HTTP GET request. Working on getting JSON back right now.

--- script.js
+++ script.js
@@ -5,21 +5,18 @@ $(document).ready(function() {
   //used for the final URL
   var wikiURL = "";
   // used to create beginning of URL
-  var prefixURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&titles=";
+  var prefixURL =  "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=";
   var whitespaceRegex = /\s+/g;
-  var totalWhitespace = 0;
-
 
 $("#search-button").on("click", function(){
   //grab value entered in search field
   searchVal = $("#input-value").val();
 
   if(searchVal !== "") {
-    //count number of whitespace
-    totalWhitespace = searchVal.match(whitespaceRegex).length;
 
     //replace any whitespace in input value with an equivalent percent-encoding value of '%20', reassign it to the search value. will be used to construct query string's URL
     searchVal = searchVal.replace(whitespaceRegex,"%20");
+  // }
 
     wikiURL = prefixURL + searchVal;
     console.log("The wiki string is: " + wikiURL);
@@ -33,9 +30,22 @@ $("#search-button").on("click", function(){
 
 
 
-$.getJSON(wikiURL, function(json, textStatus) {
-
+$.ajax({
+  url: wikiURL,
+  type: 'GET',
+  dataType: 'json',
+})
+.done(function(json) {
+  console.log("success");
+  console.log(json);
+})
+.fail(function() {
+  console.log("error");
+})
+.always(function() {
+  console.log("complete");
 });
+
 // $.ajax({
 //   url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&',
 //   type: 'GET',
