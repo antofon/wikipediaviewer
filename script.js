5d40c8551824037dea47373106a7030925e903f0
Anwana Ntofon <nikebillions70@gmail.com>
2017-12-06 20:22:42 -0800

    Grabbing value from input field and constructing a URL string query for the request to be made

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
 $(document).ready(function() {
   "use strict";
-  console.log("DOM content loaded.")
+  console.log("DOM content loaded.");
+  var searchVal = "";
+  //used for the final URL
+  var wikiURL = "";
+  // used to create beginning of URL
+  var prefixURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&titles=";
+  var whitespaceRegex = /\s+/g;
+  var totalWhitespace = 0;
 
 
-$.ajax({
-  url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&',
-  type: 'GET',
-  dataType: 'json'
-})
-.done(function(json, textStatus) {
-  console.log("success");
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
+$("#search-button").on("click", function(){
+  //grab value entered in search field
+  searchVal = $("#input-value").val();
+
+  if(searchVal !== "") {
+    //count number of whitespace
+    totalWhitespace = searchVal.match(whitespaceRegex).length;
+
+    //replace any whitespace in input value with an equivalent percent-encoding value of '%20', reassign it to the search value. will be used to construct query string's URL
+    searchVal = searchVal.replace(whitespaceRegex,"%20");
+
+    wikiURL = prefixURL + searchVal;
+    console.log("The wiki string is: " + wikiURL);
+  }
+
+  else {
+    console.log("Search value empty.");
+  }
+
 });
+
+
+
+$.getJSON(wikiURL, function(json, textStatus) {
+
+});
+// $.ajax({
+//   url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&',
+//   type: 'GET',
+//   dataType: 'json'
+// })
+// .done(function(json, textStatus) {
+//   console.log("success");
+// })
+// .fail(function() {
+//   console.log("error");
+// })
+// .always(function() {
+//   console.log("complete");
+// });
 
 
 });
